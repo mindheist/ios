b1d5f0a8d6431ff6b7a3febc39b6a9c1f2f65507
mindheist <dragonzsnake@gmail.com>
2017-12-16 19:07:35 -0800

    added IBOutlets to the score and round labels
    there is some int/float conversion stuff going on .. this needs to get
    fixed.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var target_label: UILabel!
+    @IBOutlet weak var round_label: UILabel!
     
     var current_value : Int = 0
-    var targetValue : Int = 0
+    var target_value : Int = 0
+    var difference : Int = 0
     var score : Int = 0
-    var round : Int = 0
+    var round_value : Int = 0
  
     override func viewDidLoad() {
         super.viewDidLoad()
-        current_value = Int(slider.value) // This is for the very first time only I guess,t
+        current_value = lroundf(slider.value) // This is for the very first time only I guess,t
         startNewRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -30,27 +33,42 @@ class ViewController: UIViewController {
     
     func startNewRound(){
         //generate a new random number
-        targetValue = getRandomNumber()
+        target_value = getRandomNumber()
+        target_label.text = String(target_value)
+        round_label.text = String(round_value)
         current_value = 50
         slider.value = Float(current_value)
+
     }
     
     func getRandomNumber() -> Int {
         return Int(arc4random_uniform(100)+1)
     }
     @IBAction func SliderMoved(_ slider: UISlider) {
-        current_value = Int(slider.value)
+        current_value = lroundf(slider.value)
         print("The slider value is \(current_value)")
 
     }
     
     @IBAction func showAlert(){
-        var alert_message : String = "You slided upto ... \(current_value)" + "\n" + "The target Value is \(targetValue)"
+        if Float(current_value) == Float(target_value){
+            var alert_message : String = "Bingo ! That was Spot on"
+        }
+        else if target_value > current_value{
+            difference = target_value - current_value
+        }
+        else if current_value > target_value{
+            difference = current_value - target_value
+        }
+        
+        var alert_message : String = "You slided upto ... \(current_value) and" + " the target Value is \(target_value)" + "\n" + "That was close, you missed it by \(difference)"
         let alert = UIAlertController(title: "Hello World", message: alert_message , preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default , handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         startNewRound()
+        round_value = round_value + 1 // not sure where to increment the round variable. This seems to get increment even before the user dismisses the alert
+
     }
     
 }
